d63b55fd83666a0ee8477d4761e8018745606e05
Knoxulla <jyf20034@hotmail.com>
2024-04-18 01:46:21 -0400

    spawner works

--- Assets/Scripts/NumberController.cs
+++ Assets/Scripts/NumberController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class NumberController : MonoBehaviour
 {
+    public int startMoney = 0;
+    public float startHealth = 10;
+
     public TextMeshProUGUI money;
     public TextMeshProUGUI influence;
     public TextMeshProUGUI influenceGain;
     public TextMeshProUGUI morale;
     public TextMeshProUGUI health;
-    public TextMeshProUGUI satiation;
+    //public TextMeshProUGUI satiation;
     public static string HEALTHSTAT = "health";
     public static string INFLUENCESTAT = "influence";
     public static string FOODLEFT = "satiation";
@@ -20,13 +23,24 @@ public class NumberController : MonoBehaviour
 
     public bool combatActive;
 
+    void SetInit() 
+    {
+        PlayerPrefs.SetInt(MONEYSTAT, startMoney);
+        PlayerPrefs.SetInt(INFLUENCESTAT, 0);
+        PlayerPrefs.SetInt(MORALESTAT, 0);
+        PlayerPrefs.GetFloat(HEALTHSTAT, startHealth);
+        PlayerPrefs.SetInt(DUDECOUNTER, 1);
+    }
+
     private void Start()
     {
+        SetInit();
+
         money.text = "" +PlayerPrefs.GetInt(MONEYSTAT);
         influence.text = ""+PlayerPrefs.GetInt(INFLUENCESTAT);
         morale.text = "" + PlayerPrefs.GetInt(MORALESTAT);
         health.text = PlayerPrefs.GetFloat(HEALTHSTAT) + "%";
-        satiation.text = PlayerPrefs.GetFloat(FOODLEFT) + "%";
+        //satiation.text = PlayerPrefs.GetFloat(FOODLEFT) + "%";
         StartCoroutine(GameTick());
     }
     IEnumerator GameTick()
@@ -39,15 +53,24 @@ public class NumberController : MonoBehaviour
         PlayerPrefs.SetInt(INFLUENCESTAT, PlayerPrefs.GetInt(INFLUENCESTAT) + PlayerPrefs.GetInt(DUDECOUNTER));
         influenceGain.text = "+ " + PlayerPrefs.GetInt(DUDECOUNTER);
         PlayerPrefs.SetFloat(FOODLEFT, PlayerPrefs.GetFloat(FOODLEFT) - 1);
-        if (combatActive)
+
+        if (combatActive && PlayerPrefs.GetInt(DUDECOUNTER) <= 0)
         {
             PlayerPrefs.SetFloat(HEALTHSTAT, PlayerPrefs.GetFloat(HEALTHSTAT) - 1);
+            if (PlayerPrefs.GetFloat(HEALTHSTAT) <= 0) GameOver(); // end game
         }
-        money.text = "" + PlayerPrefs.GetInt(MONEYSTAT);
+
+        money.text = "$" + PlayerPrefs.GetInt(MONEYSTAT);
         influence.text = "" + PlayerPrefs.GetInt(INFLUENCESTAT);
         morale.text = "" + PlayerPrefs.GetInt(MORALESTAT);
         health.text = PlayerPrefs.GetFloat(HEALTHSTAT) + "%";
-        satiation.text = PlayerPrefs.GetFloat(FOODLEFT) + "%";
+        //satiation.text = PlayerPrefs.GetFloat(FOODLEFT) + "%";
+
         StartCoroutine(GameTick());
+    }
+
+    void GameOver() 
+    { 
+    
     }
 }
